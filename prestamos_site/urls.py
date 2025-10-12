1baca0419d7b3b36065beb32fb1c79e57725422f
Villalobos <jmvillalobos87@gmail.com>
2025-10-12 10:57:05 -0600

    Login4

--- prestamos_site/urls.py
+++ prestamos_site/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-# Redirige la raíz al login
 def home_redirect(request):
-    return redirect('prestamos:login')  # namespace 'prestamos' + name 'login'
+    return redirect("prestamos:login")
 
 urlpatterns = [
-    path('', home_redirect, name='home'),       # raíz
-    path('admin/', admin.site.urls),            # admin
-    path('', include('prestamos.urls', namespace='prestamos')),  # incluye app con namespace
+    path("admin/", admin.site.urls),
+    path("", home_redirect, name="home"),  # raíz redirige al login
+    path("", include("prestamos.urls")),   # incluye las rutas de la app
 ]
